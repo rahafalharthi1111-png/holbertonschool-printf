5fe0dfa52831e465de39904c88a4f612b19d7081
raneemts <raneem_alsaqat@hotmail.com>
2025-10-01 14:48:18 +0000

    aa

--- main.h
+++ main.h
@@ -1,12 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include<stdarg.h>
-#include<unistd.h>
-int _printf(const char *format, ...);
+
+#include <stdarg.h>
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+
 int print_string(char *str);
 int print_number(int n);
-int print_unsigned(unsigned int n);
-int _octal(unsigned int n);
-int print_hex(unsigned int n, int upperCase);
-#endif
+
+#endif /* MAIN_H */
