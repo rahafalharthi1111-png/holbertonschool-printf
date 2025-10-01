96923505380f869014f1a3274fdfe5fa4e5dfa20
raneemts <raneem_alsaqat@hotmail.com>
2025-10-01 16:31:25 +0000

    ff

--- _printf.c
+++ _printf.c
@@ -8,120 +8,131 @@
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 /**
  * print_string - Prints a string to stdout
- * @str: Pointer to the string to print
+ * @str: Pointer to string
  *
- * Return: The number of characters printed
+ * Return: Number of characters printed
  */
 static int print_string(char *str)
 {
-    int count = 0;
+	int count = 0;
 
-    if (str == NULL)
-        str = "(null)";
+	if (str == (char *)0)
+		str = "(null)";
 
-    while (*str)
-    {
-        count += _putchar(*str);
-        str++;
-    }
-
-    return (count);
+	while (*str)
+	{
+		count += _putchar(*str);
+		str++;
+	}
+	return (count);
 }
 
 /**
  * print_number - Prints an integer to stdout
  * @n: Integer to print
  *
- * Return: The number of characters printed
+ * Return: Number of characters printed
  */
 static int print_number(int n)
 {
-    int count = 0;
-    unsigned int num = 0;
+	int count = 0;
+	unsigned int num = 0;
 
-    if (n < 0)
-    {
-        count += _putchar('-');
-        num = (unsigned int)(-n);
-    }
-    else
-    {
-        num = (unsigned int)n;
-    }
+	if (n < 0)
+	{
+		count += _putchar('-');
+		num = (unsigned int)(-n);
+	}
+	else
+	{
+		num = (unsigned int)n;
+	}
 
-    if (num / 10)
-        count += print_number((int)(num / 10));
+	if (num / 10)
+		count += print_number((int)(num / 10));
 
-    count += _putchar((char)((num % 10) + '0'));
+	count += _putchar((char)((num % 10) + '0'));
+	return (count);
+}
 
-    return (count);
+/**
+ * handle_specifier - Handles a single conversion specifier
+ * @fmt: Pointer to current format char (advanced inside)
+ * @ap:  va_list with arguments
+ *
+ * Return: Number of characters printed, or -1 on error
+ */
+static int handle_specifier(const char **fmt, va_list *ap)
+{
+	int printed = 0;
+
+	if (**fmt == '\0')
+		return (-1);
+
+	if (**fmt == 'c')
+		printed += _putchar((char)va_arg(*ap, int));
+	else if (**fmt == 's')
+		printed += print_string(va_arg(*ap, char *));
+	else if (**fmt == '%')
+		printed += _putchar('%');
+	else if (**fmt == 'd' || **fmt == 'i')
+		printed += print_number(va_arg(*ap, int));
+	else
+	{
+		printed += _putchar('%');
+		printed += _putchar(**fmt);
+	}
+
+	(*fmt)++;
+	return (printed);
 }
 
 /**
  * _printf - Produces output according to a format
- * @format: Format string containing directives
+ * @format: Format string
  *
- * Return: Number of characters printed (excluding the null byte),
- * or -1 on error (NULL format or stray '%' at end)
+ * Return: Number of chars printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-    va_list ap;
-    int count = 0;
+	va_list ap;
+	int count = 0;
 
-    if (format == NULL)
-        return (-1);
+	if (format == (const char *)0)
+		return (-1);
 
-    va_start(ap, format);
+	va_start(ap, format);
 
-    while (*format)
-    {
-        if (*format != '%')
-        {
-            count += _putchar(*format);
-            format++;
-            continue;
-        }
+	while (*format)
+	{
+		if (*format != '%')
+		{
+			count += _putchar(*format);
+			format++;
+			continue;
+		}
 
-        /* skip '%' */
-        format++;
+		format++; /* skip '%' */
 
-        if (*format == '\0')
-        {
-            va_end(ap);
-            return (-1);
-        }
+		if (*format == '\0')
+		{
+			va_end(ap);
+			return (-1);
+		}
 
-        if (*format == 'c')
-        {
-            count += _putchar((char)va_arg(ap, int));
-        }
-        else if (*format == 's')
-        {
-            count += print_string(va_arg(ap, char *));
-        }
-        else if (*format == '%')
-        {
-            count += _putchar('%');
-        }
-        else if (*format == 'd' || *format == 'i')
-        {
-            count += print_number(va_arg(ap, int));
-        }
-        else
-        {
-            count += _putchar('%');
-            count += _putchar(*format);
-        }
+		count += handle_specifier(&format, &ap);
+		if (count < 0)
+		{
+			va_end(ap);
+			return (-1);
+		}
+	}
 
-        format++;
-    }
-
-    va_end(ap);
-    return (count);
+	va_end(ap);
+	return (count);
 }
